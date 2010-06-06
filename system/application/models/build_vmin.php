a6874daea32d771721d832605afa6949d309e27a
Freez Meinster <freez_meinster@yahoo.co.id>
2010-06-06 17:45:04 +0700

    udah bener buat VPS nya

--- system/application/models/build_vmin.php
+++ system/application/models/build_vmin.php
@@ -11,21 +11,20 @@ class Build_vmin extends Model {
     $vhome = $this->config->item('vmin_home');
     $home = trim($vhome);
     $dir = trim($vdir);
-    $vps_dir = "$vdir/etc/vservers/$name";
+    $vps_dir = "$dir/etc/vservers/$name";
     $memory = $mem*250;
     $en_pass = shell_exec("openssl passwd $pass");
     $lah = trim($en_pass);
     shell_exec ("sudo $home/../script/build $name $context $ip");
-    shell_exec ("sudo /bin/echo \"$memory\" > $vps_dir/rlimits/rss.hard");
     shell_exec ("sudo /sbin/installpkg -root $home/$name $pkg/*");
     shell_exec ("sudo /bin/cp /etc/resolv.conf $home/$name/etc/");
     shell_exec ("sudo /bin/cp /etc/localtime $home/$name/etc/");
     shell_exec ("sudo /bin/cp $home/../script/fstab $home/$name/etc/ ");                                                          
     shell_exec ("sudo /bin/cp $home/../script/mtab $home/$name/etc/");
-    shell_exec ("sudo /usr/local/sbin/vserver $name exec ldconfig");
+    shell_exec ("sudo /usr/bin/chroot $home/$name/ ldconfig");
     shell_exec ("sudo /bin/cp $home/../script/rc $home/$name/etc/init.d/");  
-    shell_exec ("sudo /usr/local/sbin/vserver $name exec usermod -p $lah root");
-    shell_exec ("sudo /bin/echo \"ListenAddress $ip\" >> $home/$name/etc/ssh/sshd_config ");
-    }
+    shell_exec ("sudo /usr/bin/chroot $home/$name/ usermod -p $lah root");
+    shell_exec ("sudo  $home/../script/post_configure $ip $home/$name/ $memory $vps_dir");
+   }
 }
 ?>
